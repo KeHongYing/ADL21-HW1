ec634139f34fbcff8bc3494a13b4ffc80addedb8
KeHongYing <b06902074@ntu.edu.tw>
2021-04-10 17:29:33 +0800

    add test script

--- script/intent_cls.sh
+++ script/intent_cls.sh
@@ -1,1 +1,1 @@
-python3.8 src/test_intent.py --test_file "${1}" --ckpt_path ckpt/intent/best.pt --pred_file "${2}"
+python3.8 src/test_intent.py --test_file "${1}" --ckpt_path ckpt/intent/model_best.pt --pred_file "${2}" --device cuda:0
---
+++ script/slot_cls.sh
@@ -0,0 +1,1 @@
+python3.8 src/test_slot.py --test_file "${1}" --ckpt_path ckpt/slot/model_best.pt --pred_file "${2}" --device cuda:0
